265173b1e75d7e528a159d1c6d35d1f81e40ac10
justin zhang <judtin@seas.upenn.edu>
2021-10-13 00:28:25 -0400

    created posts component and tested on App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
  
-const App = () => (
-  <h1>Your react app!</h1>
-)
+import Posts from './Posts'
+const App = () => {
+  const [name, setName] = useState('')
+  const [post, setPost] = useState('')
+
+  return (
+    <div>
+    <input type='text' value={name} onChange={e => setName(e.target.value)}/>
+    <input type='text' value={post} onChange={e => setPost(e.target.value)}/>
+    <h1>{name}</h1>
+    <h1>{post}</h1>
+    </div>
+  )
+}
 export default App
---
+++ src/components/Posts.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Posts = ({name, post}) => {
+    return (
+        <div>{name}: {post}</div>
+    )
+}
